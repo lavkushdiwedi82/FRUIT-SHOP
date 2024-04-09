ed8fda223dc24f4bab362e98ab839ade3857326d
lavkushdiwedi82 <130441676+lavkushdiwedi82@users.noreply.github.com>
2024-04-09 11:31:44 +0530

    Add files via upload

---
+++ fruit.js
@@ -0,0 +1,23 @@
+// const mainHeading=document.getElementById('main-heading');
+// mainHeading.textContent='Fruit World';
+// mainHeading.style.text.color='Orange';
+// mainHeading.style.backgroundColor='green';
+// mainHeading.style.borderBottom='3px solid-orange';
+
+// const basketHeading = document.getElementById('basket-heading');
+// basketHeading.style.textColor ='green';
+// const thanks=document.getElementById('thanks');
+// thanks.innerHTML='<p>Please visit us again.</p>';
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent='Fruit World';
+mainHeading.style.color='orange';
+
+const header=document.getElementById('header');
+header.style.backgroundColor='green';
+header.style.borderBottom='3px solid orange';
+
+const basketHeading = document.getElementById('basket-heading');
+basketHeading.style.color = 'green';
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML='<p>Please visit us again</P>';
